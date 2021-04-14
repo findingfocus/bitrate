15d524a7328adc168214904b9fbf8b04ee2c46d9
yakulbeansMacLaptop <yakulbeans@gmail.com>
2021-04-14 14:47:26 -0600

    user can choose whether filesize is gigabytes or megabytes

--- bitrate.c
+++ bitrate.c
@@ -10,7 +10,7 @@ int main (void) {
 	fourthDigit = 0;
 	fifthDigit = 0;
 	sixthDigit = 0;
-
+/*
 	printf("Enter Duration hh:mm:ss\n");
 	printf("Enter hh\n");
 
@@ -29,17 +29,49 @@ int main (void) {
 	secondDigit = hours % 10;
 	hours = hours / 10;
 	firstDigit = hours % 10;
-	printf("First digit is %i\nSecond digit is %i\n", firstDigit, secondDigit);
+	//printf("First digit is %i\nSecond digit is %i\n", firstDigit, secondDigit);
 
 	fourthDigit = minutes % 10;
 	minutes = minutes / 10;
 	thirdDigit = minutes % 10;
-	printf("Third digit is %i\nFourth digit is %i\n", thirdDigit, fourthDigit);
+	//printf("Third digit is %i\nFourth digit is %i\n", thirdDigit, fourthDigit);
 
 	sixthDigit = seconds % 10;
 	seconds = seconds / 10;
 	fifthDigit = seconds % 10;
-	printf("Fifth digit is %i\nSixth digit is %i\n", fifthDigit, sixthDigit);
+	//printf("Fifth digit is %i\nSixth digit is %i\n", fifthDigit, sixthDigit);
+
+
+
+*/
+
+	//Receives Filesize
+
+	//ask to input until answer is either g or m,
+	char filetype;
+	float filesize;
+	float megabytes;
+	float gigabytes;
+
+	do {
+		printf("Filesize: Enter g for gigabytes or m for megabytes\n");
+		scanf(" %c", &filetype);
+	} while (filetype != 'm' && filetype != 'g');
+
+	if (filetype == 'm')
+	{
+		printf("How many megabytes?\n");
+		scanf("%f", &megabytes);
+		printf("%.2f megabytes\n", megabytes);
+	}
+	if (filetype == 'g')
+	{
+		printf("How many gigabytes?\n");
+		scanf("%f", &gigabytes);
+		printf("%.2f gigabytes\n", gigabytes);
+	}
+	//printf("exit\n");
+
 
 
 
